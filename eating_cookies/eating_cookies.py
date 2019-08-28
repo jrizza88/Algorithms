df84900d6a5571662891a569027aff6025c6b1b2
jrizza88 <jetorres88@gmail.com>
2019-08-27 21:59:54 -0400

    solved eating_Cookies without caching

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python
 
 import sys
+def eating_cookies(n, cache=None):
+  # pass
+  if n < 0:
+    return 0
+  elif n == 0 or n == 1:
+    return 1
+  else:
+    val = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+    print(f'value: {val}')
+    return val
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+# def eating_cookies(n, cache=None):
+#   # pass
+#   # print(f'printing n: {n}')
+#   # if n == 0 or n == 1:
+#   #   return 1
+#   if n < 0:
+#     return 0
+#   elif n == 0 or n == 1:
+#     return 1
+#   else:
+#     if cache[n] > 0:
+#       # print(f' first if statement...: {cache[n]}')
+#       return cache[n]
+#     else:
+#       val = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+#       # print(f'value: {val}')
+#       print(f'cache with n passed to it: {cache[n]}')
+#       cache[n] = val
+#       return val
+
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
