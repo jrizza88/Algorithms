ca4ea98570c30708a5cece1ea2437b7bd63399cb
jrizza88 <jetorres88@gmail.com>
2019-08-27 23:26:02 -0400

    progress on the first question

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,19 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  for i in range(0, len(prices) - 1):
+    current_min_price_so_far = i
+    # max_profit_so_far
+
+    for j in range(current_min_price_so_far, len(prices) - 2):
+      max_profit_so_far = j 
+      if prices[max_profit_so_far] < prices[current_min_price_so_far]:
+        print(f'lowest is {prices[current_min_price_so_far]}, maximum is {prices[current_min_price_so_far]}')
+
+        new_max_profit = max_profit_so_far
+        max_profit_so_far = current_min_price_so_far
+        print(f'old max profit is {prices[max_profit_so_far]}, new is {prices[new_max_profit]}')
+        
 
 
 if __name__ == '__main__':
